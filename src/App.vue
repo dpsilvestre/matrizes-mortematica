175fcded93d2114e3a6dca439f014899f51015a2
dpsilvestre <dp2132332@gmail.com>
2023-06-16 15:05:02 -0300

    matriz

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const um = prompt("primeira")
+const dois = prompt("segundo")
+const tres = prompt("terceiro")
+
+let array = [[um,dois,tres]];
+console.log(array);
+</script>
+
+<template>
+ <p>a</p>
+</template>
+
+<style scoped>
+
+</style>
