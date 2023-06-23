8281ebb5da67e1572a986c1c62ec3c7bc6d5a562
dpsilvestre <dp2132332@gmail.com>
2023-06-23 14:56:33 -0300

    eu gosot dela

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <script setup>
-const um = prompt("primeira")
-const dois = prompt("segundo")
-const tres = prompt("terceiro")
 
-let array = [[um,dois,tres]];
-console.log(array);
+  import { ref } from 'vue'
+
+function lerMatriz() {
+  const matriz = []
+  const linhas = parseInt(prompt('Informe o número de linhas da matriz:'))
+  const colunas = parseInt(prompt('Informe o número de colunas da matriz:'))
+
+  for (let i = 0; i < linhas; i++) {
+    const linha = []
+
+    for (let j = 0; j < colunas; j++) {
+      const valor = parseInt(prompt(`Informe o valor para a posição [${i + 1}][${j + 1}]:`))
+      linha.push(valor)
+    }
+
+    matriz.push(linha)
+  }
+
+  return matriz
+}
+
+function calcularSomaTraco(matriz) {
+  const linhas = matriz.length
+  const colunas = matriz[0].length
+  if (linhas !== colunas) {
+    return 'A matriz deve ser quadrada .'
+  } else {
+    let soma = 0
+    for (let i = 0; i < linhas; i++) {
+      soma += matriz[i][i]
+    }
+    return soma
+  }
+}
+
+const matriz = lerMatriz()
+
+const somaTraco = calcularSomaTraco(matriz)
+
+const resposta = ref(null)
+
+function mostrarTransposta() {
+
+  if (typeof somaTraco === typeof 1) {
+    resposta.value = `O traço da matriz é: ${somaTraco}`
+  } else {
+    resposta.value = `Erro: ${somaTraco}`
+  }
+}
+
+mostrarTransposta()
 </script>
 
 <template>
- <p>a</p>
+  <p>{{ resposta }}</p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
